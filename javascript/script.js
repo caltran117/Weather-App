0f74dea25de935062ffe89cd2bce8d8205a32a18
Calvin <84696281+caltran117@users.noreply.github.com>
2022-10-16 21:41:35 -0400

    query sel., event listener

--- javascript/script.js
+++ javascript/script.js
@@ -1,16 +1,22 @@
 // TODO: Edit the URL to get only 5 issues of Twitter's Chill repo
 
 
+var inputval = document.querySelector('#cityinput')
+var submitbtn = document.querySelector('#add')
 
 
-var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q={philadelphia}&appid={0bcbdea2eca98a28bdc8b794c845d739}';
+submitbtn.addEventListener('click',function(){
 
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q={+inputval.value+}&appid={0bcbdea2eca98a28bdc8b794c845d739}';
 
+    fetch(requestUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+      });
+    
 
-fetch(requestUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
+})
+
